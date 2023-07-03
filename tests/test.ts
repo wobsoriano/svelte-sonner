93fdce2dd52f06fb37d4e1268b0c2b5e56e54db3
wobsoriano <sorianorobertc@gmail.com>
2023-07-02 22:13:50 -0700

    test: more basic tests

--- tests/test.ts
+++ tests/test.ts
@@ -11,7 +11,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Basic functionality', () => {
   test('toast is rendered and disappears after the default timeout', async ({ page }) => {
-    await page.getByTestId('hero-button').click()
+    await page.getByTestId('default-button').click()
     await expect(page.locator('[data-sonner-toast]')).toHaveCount(0)
     await expect(page.locator('[data-sonner-toast]')).toHaveCount(0)
   })
@@ -39,11 +39,19 @@ test.describe('Basic functionality', () => {
   // })
 
   test('toast is removed after swiping down', async ({ page }) => {
-    await page.getByTestId('hero-button').click()
+    await page.getByTestId('default-button').click()
     await page.hover('[data-sonner-toast]')
     await page.mouse.down()
     await page.mouse.move(0, 800)
     await page.mouse.up()
     await expect(page.locator('[data-sonner-toast]')).toHaveCount(0)
+  })
+
+	test('toast is not removed when hovered', async ({ page }) => {
+    await page.getByTestId('default-button').click()
+    await page.hover('[data-sonner-toast]')
+    const timeout = new Promise(resolve => setTimeout(resolve, 5000))
+    await timeout
+    await expect(page.locator('[data-sonner-toast]')).toHaveCount(1)
   })
 })
