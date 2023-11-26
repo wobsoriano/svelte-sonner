749b02dfee65fc8db0e424af7fa02176560a81df
wobsoriano <sorianorobertc@gmail.com>
2023-11-26 14:14:05 -0800

    update warning and info colors

--- src/lib/Toaster.svelte
+++ src/lib/Toaster.svelte
@@ -606,17 +606,17 @@ function handleFocus(event: FocusEvent & {
     --success-border: hsl(145, 92%, 91%);
     --success-text: hsl(140, 100%, 27%);
 
+    --info-bg: hsl(208, 100%, 97%);
+    --info-border: hsl(221, 91%, 91%);
+    --info-text: hsl(210, 92%, 45%);
+
+    --warning-bg: hsl(49, 100%, 97%);
+    --warning-border: hsl(49, 91%, 91%);
+    --warning-text: hsl(31, 92%, 45%);
+
     --error-bg: hsl(359, 100%, 97%);
     --error-border: hsl(359, 100%, 94%);
     --error-text: hsl(360, 100%, 45%);
-
-    --info-bg: hsl(210, 85%, 96%);
-    --info-border: hsl(210, 92%, 91%);
-    --info-text: hsl(210, 100%, 27%);
-
-    --warning-bg: hsl(60, 85%, 96%);
-    --warning-border: hsl(60, 92%, 91%);
-    --warning-text: hsl(60, 100%, 19%);
   }
 
   [data-sonner-toaster][data-theme='light'] [data-sonner-toast][data-invert='true'] {
@@ -640,17 +640,17 @@ function handleFocus(event: FocusEvent & {
     --success-border: hsl(147, 100%, 12%);
     --success-text: hsl(150, 86%, 65%);
 
+    --info-bg: hsl(215, 100%, 6%);
+    --info-border: hsl(223, 100%, 12%);
+    --info-text: hsl(216, 87%, 65%);
+
+    --warning-bg: hsl(64, 100%, 6%);
+    --warning-border: hsl(60, 100%, 12%);
+    --warning-text: hsl(46, 87%, 65%);
+
     --error-bg: hsl(358, 76%, 10%);
     --error-border: hsl(357, 89%, 16%);
     --error-text: hsl(358, 100%, 81%);
-
-    --info-bg: hsl(210, 100%, 6%);
-    --info-border: hsl(207, 100%, 12%);
-    --info-text: hsl(210, 86%, 65%);
-
-    --warning-bg: hsl(60, 100%, 6%);
-    --warning-border: hsl(57, 100%, 12%);
-    --warning-text: hsl(60, 86%, 65%);
   }
 
   [data-rich-colors='true'] [data-sonner-toast][data-type='success'] {
