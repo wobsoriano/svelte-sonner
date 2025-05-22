5e678145bb36e7629c606a59bf063cd877ce4344
wobsoriano <sorianorobertc@gmail.com>
2025-05-21 18:54:18 -0700

    chore: type fix

--- src/tests/ToastTest.svelte
+++ src/tests/ToastTest.svelte
@@ -1,14 +1,6 @@
-<script lang="ts" module>
-	type ToastFn = (t: typeof toast) => void;
-
-	export type ToastTestProps = {
-		cb: ToastFn;
-	};
-</script>
-
 <script lang="ts">
 	import { Toaster, toast } from '$lib/index.js';
-	const { cb }: ToastTestProps = $props();
+	const { cb }: { cb: (t: typeof toast) => void } = $props();
 
 	function onClick() {
 		cb(toast);
