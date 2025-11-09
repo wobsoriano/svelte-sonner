cbc4a329ff41118150f7b52bc5c7f3955d2a165c
wobsoriano <sorianorobertc@gmail.com>
2025-11-09 09:33:25 -0800

    chore: remove unused code

--- src/lib/Toast.svelte
+++ src/lib/Toast.svelte
@@ -186,12 +186,7 @@
 
 		initialHeight = finalHeight;
 
-		// setHeight reads heights and toasts state. Untrack the call
-		// to avoid triggering this effect when those are modified. e.g. toasts
-		// added and removed.
-		// untrack(() => {
 		toastState.setHeight({ toastId: toast.id, height: finalHeight });
-		// });
 	});
 
 	function deleteToast() {
