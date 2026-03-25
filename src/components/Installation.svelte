f49e71504f217ad9d93ddde1d39bd8b302365e14
Sukesh Pabolu IN <sukeshpabolu@Sukeshs-Mac-mini.local>
2026-03-25 22:04:53 +0100

    chore: migrate to runes.

--- src/components/Installation.svelte
+++ src/components/Installation.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import copy from 'copy-to-clipboard';
 
-	let copying = 0;
+	let copying = $state(0);
 
 	function onCopy() {
 		copy('npm install svelte-sonner');
@@ -14,8 +14,8 @@
 
 <div>
 	<h2>Installation</h2>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 	<code class="code" onclick={onCopy}>
 		npm install svelte-sonner
 		<button aria-label="Copy code" class="copy">
